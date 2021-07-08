b426a83acdc04a3fb050ec07aeb880e99f28fb6a
paul-shannon <paul.thurmond.shannon@gmail.com>
2021-07-08 13:20:12 -0700

    standardized clinical features table now produced

--- R/endophenotypeExplorer.R
+++ R/endophenotypeExplorer.R
@@ -3,6 +3,7 @@
 #' @importFrom RPostgreSQL dbConnect dbListTables dbGetQuery dbListConnections dbDisconnect
 #' @import RPostgreSQL
 #' @import org.Hs.eg.db
+#' @import TxDb.Hsapiens.UCSC.hg38.knownGene
 #' @import R6
 #' @import VariantAnnotation
 #' @import GenomicRanges
@@ -23,15 +24,28 @@
 #' An endophenotypeExplore has access to eQTLs, variant calls, and more
 #'
 #' @export
+#'
 EndophenotypeExplorer = R6Class("EndophenotypeExplorer",
+
     #--------------------------------------------------------------------------------
     private = list(target.gene=NULL,
+                   chromosome=NULL,
                    vcf.url=NULL,
                    geneReg.db=NULL,
-                   default.genome=NULL
+                   default.genome=NULL,
+                   tbl.clinical=NULL,
+                   tbl.idMap=NULL,
+                   tbl.biospecimen.rosmap=NULL,
+                   tbl.biospecimen.mayo=NULL,
+                   tbl.biospecimen.sinai=NULL,
+                   tbl.clinical.rosmap=NULL,
+                   tbl.clinical.sinai=NULL,
+                   tbl.clinical.mayo=NULL
                    ),
+
     #--------------------------------------------------------------------------------
     public = list(
+
       #' @description
       #' Create a new EndophenotypeExplorer object
       #' @param target.gene  Gene of interest.
@@ -39,11 +53,141 @@ EndophenotypeExplorer = R6Class("EndophenotypeExplorer",
       #' @param vcf.url https endpoint from serving indexed vcf files
       #' @return A new `EndophenotypeExplorer` object.
 
-        initialize = function(target.gene, default.genome, vcf.url){
+        initialize = function(target.gene, default.genome){
             private$target.gene <- target.gene
             private$default.genome <- default.genome
-            private$vcf.url <- vcf.url
+            private$chromosome <- self$identifyTargetGeneChromosome(target.gene)
+            private$vcf.url <- self$setupVcfURL(private$chromosome)
+            self$setupClinicalData()
+            },
+
+        setupVcfURL = function(chromosome){
+           vcf.base.url <- "https://igv-data.systemsbiology.net/static/ampad"
+           vcf.directory <- "NIA_JG_1898_samples_GRM_WGS_b37_JointAnalysis01_2017-12-08_recalibrated_variants"
+           sprintf("%s/%s/%s.vcf.gz", vcf.base.url, vcf.directory, chromosome)
+           },
+
+        getVcfUrl = function(){
+           private$vcf.url
+           },
+
+        getGenoMatrix = function(chrom, start, end){
+            roi <- GRanges(seqnames=chrom, IRanges(start=start, end=end))
+            x <- readVcf(private$vcf.url, private$default.genome, roi)
+            stopifnot("GT" %in% names(geno(x)))
+            invisible(geno(x)$GT)
+            },
+
+        getIdMap = function(){
+           private$tbl.idMap
+           },
+
+        mapSampleIdToPatientAndCohort = function(sampleID){
+           return(subset(private$tbl.idMap, vcf==sampleID))
+           },
+
+        setupClinicalData = function(){
+               # first the per-sample biospecimen data
+            dir <- system.file(package="EndophenotypeExplorer", "extdata", "biospecimen")
+            private$tbl.biospecimen.rosmap <- read.table(file.path(dir, "ROSMAP_biospecimen_metadata.csv"),
+                                                         sep=",", header=TRUE, as.is=TRUE)
+            private$tbl.biospecimen.sinai <- read.table(file.path(dir, "MSBB_biospecimen_metadata.csv"),
+                                                        sep=",", header=TRUE, as.is=TRUE)
+            private$tbl.biospecimen.mayo <- read.table(file.path(dir, "MayoRNAseq_biospecimen_metadata.csv"),
+                                                       sep=",", header=TRUE, as.is=TRUE)
+              # now the per-patient clinical data
+            dir <- system.file(package="EndophenotypeExplorer", "extdata", "clinical")
+            private$tbl.clinical.rosmap <- read.table(file.path(dir, "ROSMAP_clinical.csv"),
+                                                      sep=",", header=TRUE, as.is=TRUE)
+            private$tbl.clinical.sinai <- read.table(file.path(dir, "MSBB_individual_metadata.csv"),
+                                                      sep=",", header=TRUE, as.is=TRUE)
+            private$tbl.clinical.mayo <- read.table(file.path(dir, "MayoRNAseq_individual_metadata.csv"),
+                                                      sep=",", header=TRUE, as.is=TRUE)
+            dir <- "~/github/endophenotypeExplorer/inst/extdata/idMapping"
+               # the crucial sample-to-patient mapping
+               # this was difficult to obtain.  see the mapToPatientID function in
+               # ~/github/TrenaProjectAD/explore/ampad.eQTLS/ldlr.R
+               # todo: move this code to a prep directory in this package
+            private$tbl.idMap <- get(load(file.path(dir, "tbl.vcfToPatientIDs.RData")))
+            },
+
+        getClinicalTable = function(){
+            private$tbl.clinical
+            },
+
+        identifyTargetGeneChromosome = function(target.gene){
+           suppressMessages({
+             entrezID <- select(org.Hs.eg.db, keys=target.gene, keytype="SYMBOL", columns="ENTREZID")$ENTREZID
+             stopifnot(nchar(entrezID) > 1)
+             select(TxDb.Hsapiens.UCSC.hg38.knownGene, keys=entrezID, keytype="GENEID",
+                    columns="TXCHROM")$TXCHROM
+             })
+           },
+
+        rosmap.patient.table = function(patientID){
+            subset(private$tbl.clinical.rosmap, individualID==patientID)
+            },
+
+        sinai.patient.table = function(patientID){
+            subset(private$tbl.clinical.sinai, individualID==patientID)
+            },
+
+        mayo.patient.table = function(patientID){
+            subset(private$tbl.clinical.mayo, individualID==patientID)
+            },
+
+        vcfSampleID.to.clinicalTable = function(sampleID){
+            tbl <- subset(private$tbl.idMap, vcf==sampleID)
+            stopifnot(nrow(tbl) == 1)
+            patientID <- tbl$patient
+            cohort <- tbl$cohort
+            tbl.patient <- switch(cohort,
+              "mayo"   = self$standardizeMayoPatientTable(subset(private$tbl.clinical.mayo,
+                                                          individualID==patientID)),
+              "rosmap" = self$standardizeRosmapPatientTable(subset(private$tbl.clinical.rosmap,
+                                                            individualID==patientID)),
+              "sinai"  = self$standardizeSinaiPatientTable(subset(private$tbl.clinical.sinai,
+                                                           individualID==patientID))
+              )
+            tbl.patient$cohort <- cohort
+            tbl.patient$sampleID <- sampleID
+            coi <- c("patientID", "sampleID", "cohort", "study","sex","ethnicity","apoeGenotype",
+                     "braak","cerad","cogdx","pmi", "ageAtDeath")
+            tbl.patient[, coi]
+            },
+
+        standardizeMayoPatientTable = function(tbl){
+            coi <- c("individualID", "individualIdSource", "sex", "ethnicity", "apoeGenotype",
+                     "Braak", "CERAD", "pmi", "ageDeath")
+           tbl.0 <- tbl[, coi]
+           tbl.0$cogdx <- NA
+           standard.names <- c("patientID", "study", "sex","ethnicity","apoeGenotype","braak","cerad",
+                                "pmi", "ageAtDeath", "cogdx")
+           colnames(tbl.0) <- standard.names
+           tbl.0
+           },
+
+        standardizeRosmapPatientTable = function(tbl){
+           coi <- c("individualID", "Study", "msex", "race", "apoe_genotype", "braaksc", "ceradsc",
+                    "cogdx", "pmi", "age_death")
+           tbl.0 <- tbl[, coi]
+           standard.names <- c("patientID", "study", "sex","ethnicity","apoeGenotype","braak","cerad",
+                                "cogdx", "pmi", "ageAtDeath")
+           colnames(tbl.0) <- standard.names
+           tbl.0
+           },
+
+        standardizeSinaiPatientTable = function(tbl){
+           coi <- c("individualID","individualIdSource","sex","ethnicity","apoeGenotype",
+                    "Braak","CERAD","pmi","ageDeath")
+           tbl.0 <- tbl[, coi]
+           tbl.0$cogdx <- NA
+           standard.names <- c("patientID", "study", "sex","ethnicity","apoeGenotype","braak","cerad",
+                                "pmi", "ageAtDeath", "cogdx")
+           colnames(tbl.0) <- standard.names
+           tbl.0
            }
+
        ) # public
 
     ) # class EndophenotypeExplorer
--- inst/unitTests/test_endophenotypeExplorer.R
+++ inst/unitTests/test_endophenotypeExplorer.R
@@ -4,6 +4,8 @@ library(EndophenotypeExplorer)
 runTests <- function()
 {
     test_ctor()
+    test_getGenoMatrix()
+    test_mapSampleIdToPatientAndCohort()
     test_vcf.sampleID.to.clinicalTable()
 
 } # runTests
@@ -19,36 +21,93 @@ test_ctor <- function()
 
 } # test_ctor
 #----------------------------------------------------------------------------------------------------
+test_getGenoMatrix <- function()
+{
+   message(sprintf("--- test_getGenoMatrix"))
+
+   etx <- EndophenotypeExplorer$new("BIN1", "hg19")
+      # chr2:127,084,188-127,084,203: 16 base pairs around rs10200967 in bin1 5' utr
+   mtx.geno <- etx$getGenoMatrix("2", 127084188, 127084203)
+   checkEquals(dim(mtx.geno), c(2, 1894))
+   checkEquals(rownames(mtx.geno), c("2:127084193_G/A", "2:127084194_A/C"))
+
+} # test_getGenoMatrix
+#----------------------------------------------------------------------------------------------------
+test_mapSampleIdToPatientAndCohort <- function()
+{
+   message(sprintf("--- test_mapSampleIdToPatientAndCohort"))
+   etx <- EndophenotypeExplorer$new("BIN1", "hg19")
+   tbl.map <- etx$getIdMap()
+   checkEquals(dim(tbl.map), c(1845, 3))
+   checkEquals(colnames(tbl.map), c("vcf", "patient", "cohort"))
+
+   map <- as.list(etx$mapSampleIdToPatientAndCohort("SM-CJGH1"))
+   checkEquals(map, list(vcf="SM-CJGH1", patient="R7025378", cohort="rosmap"))
+
+} # test_mapSampleIdToPatientAndCohort
+#----------------------------------------------------------------------------------------------------
 test_vcf.sampleID.to.clinicalTable <- function()
 {
    message(sprintf("--- test_vcf.sampleID.to.clinicalTable"))
 
    etx <- EndophenotypeExplorer$new("BIN1", "hg19")
    checkTrue(all(c("EndophenotypeExplorer", "R6") %in% class(etx)))
+
+      # choose just one
    tbl.clinical <- etx$vcfSampleID.to.clinicalTable("SM-CTEMF")
-   checkEquals(dim(tbl.clinical), c(1, 36))
-
-   rs10200967.00.samples <- c("1036","11241","11249","11254","11275","11312","11337","11349","11370",
-                              "11399","11422","11470","14552","1940","1954","1963","68682","6976",
-                              "70594","71689","71736","71739","71749","71767","71779","71789","71801",
-                              "71824","71860","71884","71886","71900","71901","71903","71904","71908",
-                              "71927","71964","71974","71986","76348","894","MAP46251007","MAP50104134",
-                              "MAP93787649","SM-CJEG3","SM-CJEG4","SM-CJEGC","SM-CJEGE","SM-CJEGQ",
-                              "SM-CJEHR","SM-CJEIU","SM-CJEJI","SM-CJEKL","SM-CJEKW","SM-CJFKO",
-                              "SM-CJFKZ","SM-CJFL8","SM-CJFLL","SM-CJFN9","SM-CJFNJ","SM-CJFOC",
-                              "SM-CJGH1","SM-CJGI5","SM-CJGIP","SM-CJGJ2","SM-CJGLF","SM-CJGMT",
-                              "SM-CJGNF","SM-CJGNJ","SM-CJIXB","SM-CJIYK","SM-CJIZQ","SM-CJIZS",
-                              "SM-CJJ17","SM-CJJ1H","SM-CJJ1K","SM-CJJ38","SM-CJK3F","SM-CJK41",
-                              "SM-CJK49","SM-CTDQO","SM-CTDQR","SM-CTDQV","SM-CTDQX","SM-CTDR5",
-                              "SM-CTDSV","SM-CTDTL","SM-CTDVA","SM-CTDVG","SM-CTECN","SM-CTEE7",
-                              "SM-CTEED","SM-CTEFD","SM-CTEGU","SM-CTEID","SM-CTELW","SM-CTEMS")
-
-   tbl.clinical <- etx$vcfSampleID.to.clinicalTable(rs10200967.00.samples)
-      # todo: 98 samples ids, only 53 have clinical values
-   checkEquals(dim(tbl.clinical), c(53, 36))
+   checkEquals(dim(tbl.clinical), c(1, 18))
+   checkEquals(colnames(tbl.clinical), c("projid", "Study", "msex", "educ", "race", "spanish",
+                                         "apoe_genotype", "age_at_visit_max", "age_first_ad_dx",
+                                         "age_death", "cts_mmse30_first_ad_dx", "cts_mmse30_lv",
+                                         "pmi", "braaksc", "ceradsc", "cogdx", "dcfdx_lv", "individualID"))
+      # spot check a few important fields
+   checkEquals(tbl.clinical$apoe_genotype, 33)
+   checkEquals(tbl.clinical$braaksc, 4)
+   checkEquals(tbl.clinical$ceradsc, 2)
 
 } # test_vcf.sampleID.to.clinicalTable
 #----------------------------------------------------------------------------------------------------
+test_vcf.sampleID.to.clinicalTable <- function()
+{
+   message(sprintf("--- test_vcf.sampleID.to.clinicalTable"))
+
+   etx <- EndophenotypeExplorer$new("BIN1", "hg19")
+   tbl.map <- etx$getIdMap()
+
+   sinai.pt.id <- subset(tbl.map, cohort=="sinai")$patient[1]
+   as.data.frame(t(etx$sinai.patient.table(sinai.pt.id)))
+
+   rosmap.pt.id <- subset(tbl.map, cohort=="rosmap")$patient[1]
+   as.data.frame(t(etx$rosmap.patient.table(rosmap.pt.id)))
+
+   mayo.pt.id <- subset(tbl.map, cohort=="mayo")$patient[1]
+   as.data.frame(t(etx$mayo.patient.table(mayo.pt.id)))
+
+      # a rosmap sample
+   tbl.std <- etx$vcfSampleID.to.clinicalTable("SM-CTEGN")
+
+   vcf.ids <- c(subset(tbl.map, cohort=="sinai")$vcf[1],
+                subset(tbl.map, cohort=="rosmap")$vcf[1],
+                subset(tbl.map, cohort=="mayo")$vcf[1])
+
+   tbls <- lapply(vcf.ids, function(id) etx$vcfSampleID.to.clinicalTable(id))
+   tbl <- do.call(rbind, tbls)
+
+   # as.data.frame(t(tbl))
+   set.seed(17)
+   vcf.ids <- sample(tbl.map$vcf, size=100)
+   tbls <- lapply(vcf.ids, function(id) etx$vcfSampleID.to.clinicalTable(id))
+   tbl.all <- do.call(rbind, tbls)
+
+   checkEqualsNumeric(mean(tbl.all$braak, na.rm=TRUE), 3.9, tolerance=0.2)
+   checkEqualsNumeric(mean(tbl.all$pmi, na.rm=TRUE),    78, tolerance=0.2)
+   checkEqualsNumeric(mean(tbl.all$cogdx, na.rm=TRUE), 2.5, tolerance=0.2)
+   checkEqualsNumeric(mean(tbl.all$cerad, na.rm=TRUE), 2.3, tolerance=0.2)
+
+    checkEquals(length(which(is.na(tbl.all$cogdx))), 32)
+
+} # test_vcf.sampleID.to.clinicalTable
+#----------------------------------------------------------------------------------------------------
+
 if(!interactive())
    runTests()
-
